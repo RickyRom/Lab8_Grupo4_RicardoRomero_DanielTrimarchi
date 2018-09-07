59a38fd5b35c8fedf6b015f31c75c63216620a5d
trimarchi19 <dtrimarchi19@unitec.edu>
2018-09-07 14:12:36 -0600

    Modificando Radical

--- Radical.cpp
+++ Radical.cpp
@@ -8,21 +8,21 @@
 
 	}
 	double Radical::getCoeficiente(){
-
+		return coeficiente;
 	}
-	void Radical::setCoeficiente(double Coef){
-		
+	void Radical::setCoeficiente(double pCoef){
+		coeficiente= pCoef
 	}
 	
 	double Radical::getIndice(){
-
+		return indice;
 	}
-	void Radical::setIndice(doube pIndice){
+	void Radical::setIndice(double pIndice){
 		indice=pIndice;
 	}
 
 	double Radical::getRadicando(){
-
+		return radicando;
 	}
 	void Radical::setRadicando(double pRad){
 		radicando=pRad;
